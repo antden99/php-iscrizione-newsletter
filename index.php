cd687f47e041b8699d844c164b9cc7b2590bddc6
antden99 <antoniodenigris@icloud.com>
2024-04-16 14:07:36 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php  echo "tutto ok"   ?>
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP iscrizione newsletter</title>
+</head>
+<body>
+    <h1>NewsLetter</h1>
+</body>
+</html>
